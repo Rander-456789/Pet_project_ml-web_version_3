ca7de7ea7a94d698fe60e6bc3debd1a1c97125e1
Rander-456789 <Ilya.palkin2009@gmail.com>
2025-12-15 19:07:57 +0300

    Update service.py

--- service.py
+++ service.py
@@ -1,23 +1,26 @@
 import joblib
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+# ====== ВХОДНЫЕ ДАННЫЕ ======
 class ClientData(BaseModel):
     age: int
-    person_income: float
-    loan_amnt: float
+    person_income: float          
+    loan_amnt: float             
     loan_int_rate: float
-    person_education_encoded: str
-    person_home_ownership_encoded: str
+    person_education: str
+    person_home_ownership: str
 
+# ====== APP ======
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,17 +28,45 @@ app.add_middleware(
 
 app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 
-model = joblib.load('model_2.pkl')
+model = joblib.load("model_2.pkl")
 
 @app.get("/")
 def root():
-    return FileResponse("frontend/index3.html")
+    return FileResponse("frontend/index.html")
 
-@app.post('/score')
+# ====== PREPROCESSING ======
+def preprocess(data: ClientData) -> pd.DataFrame:
+    education_map = {
+        "Студент": 1,
+        "Бакалавр": 2,
+        "Магистр": 3
+    }
+
+    home_map = {
+        "Арендованое": 1,
+        "Собственное": 2,
+        "Ипотечное": 3
+    }
+
+    X = pd.DataFrame([{
+        "person_age": data.age,
+        "person_income": data.person_income,
+        "loan_amnt": data.loan_amnt,
+        "loan_int_rate": data.loan_int_rate,
+        "person_education": education_map[data.person_education],
+        "person_home_ownership": home_map[data.person_home_ownership]
+    }])
+
+    return X
+
+# ====== SCORING ======
+@app.post("/score")
 def score(data: ClientData):
-    features = [data.age, data.person_income, data.loan_amnt, data.loan_int_rate, data.person_education_encoded, data.person_home_ownership_encoded]
-    features[-2] = 1 if features[-2] == 'Студент' else ( 2 if features[-2] == 'Бакалавр' else 3)
-    features[-1] = 1 if features[-1] == 'Арендованое' else ( 2 if features[-1] == 'Собственное' else 3)
-    approved = not model.predict([features])[0].item()
-    return {'approved': approved}
+    X = preprocess(data)
+    pred = model.predict(X)[0]    
+    approved = bool(pred == 0)
+
+    return {
+        "approved": approved
+    }
 
